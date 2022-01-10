cf2125120dd1967b4b2992f6bbfb6b8347eb4bed
Reuben Lewis <reuben.lewis@outlook.com>
2022-01-09 23:00:24 -0800

    Updated the colors to HSV.
    This allows us to play around with the saturation and the brightness
    of each LED.

--- toolTesting.ino
+++ toolTesting.ino
@@ -6,28 +6,19 @@
 #define LED_COUNT 10
 #define MAX_MOVING 248
 #define MAX_STATIONARY 184
-#define CHANGE_COLOR 16
+#define CHANGE_COLOR 36
 #define RANGE 8
-#define FPS 30
-
-typedef struct color_t {
-    uint8_t r;
-    uint8_t g;
-    uint8_t b;
-} color_t;
+#define FPS 10
 
 typedef struct firectx {
     uint8_t index;
     uint8_t brightness;
-    color_t color;
+    uint8_t color;
     bool accending;
 } firectx;
 firectx ctx[LED_COUNT] = {0};
 
 CRGB strip[LED_COUNT];
-color_t orange = {255, 254*.1, 0};
-color_t redish = {255, 254*.2, 0};
-color_t red = {255, 0, 0};
 
 void setup() {
     randomSeed(analogRead(9)); // Just make sure that this is unconnected.
@@ -54,9 +45,9 @@ void setLed(firectx* ctx) {
     if (ctx->accending) {
         ctx->brightness = random(ctx->brightness, ctx->brightness + RANGE);
     } else {
-        ctx->brightness = random(ctx->brightness - RANGE, ctx->brightness);
+        ctx->brightness = random(ctx->brightness - RANGE, ctx->brightness + 1);
     }
-    strip[ctx->index].fadeLightBy(ctx->brightness);
+    strip[ctx->index] = CHSV(ctx->color, 255, ctx->brightness);
 
     // If the brightness is small enough, choose a different color.
     if (ctx->brightness < CHANGE_COLOR) {
@@ -65,25 +56,24 @@ void setLed(firectx* ctx) {
         ctx->brightness = CHANGE_COLOR + 1;
     } else if (ctx->brightness > MAX_STATIONARY) {
         ctx->accending = ~ctx->accending;
+    } else {
+        uint8_t val = random(10);
+        if (val == 0) {
+            ctx->accending = ~ctx->accending;
+        }
     }
-
 }
 
 void setPixelColors(firectx* ctx) {
-    #define RED_RANGE 40
-    #define REDISH_RANGE 60
+    #define RED_RANGE 80
     #define ORANGE_RANGE 100
     #define TOTAL_RANGE ORANGE_RANGE
     uint32_t color = 0;
     uint8_t value = random(TOTAL_RANGE);
     if (value < RED_RANGE) {
-        ctx->color = red;
-    } else if (value < REDISH_RANGE) {
-        ctx->color = redish;
-    } else {
-        ctx->color = orange;
+        ctx->color = random(HUE_RED, HUE_ORANGE);
+    } else if (value < ORANGE_RANGE) {
+        ctx->color = random(HUE_ORANGE, HUE_YELLOW - 16);
     }
-    strip[ctx->index].r = ctx->color.r;
-    strip[ctx->index].g = ctx->color.g;
-    strip[ctx->index].b = ctx->color.b;
+    strip[ctx->index] = CHSV(ctx->color, random(100,256), ctx->brightness);
 }
