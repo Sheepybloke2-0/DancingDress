bfdd589a0b42b2970f2bc077d1209c319bf3bf67
Reuben Lewis <reuben.lewis@outlook.com>
2022-03-27 12:57:16 -0700

    Setting this up to run on the gemma m0.
    Now that we know how to fix the issue with the M0, set the dress
    up to use that. Integrates the accelerometer and the lights changing.

--- dress/dress.ino
+++ dress/dress.ino
@@ -1,18 +1,19 @@
+#define ARDUINO_GEMMA_M0 1
 #include <FastLED.h>
 #include <Wire.h>
 #include <math.h>
 #include <Adafruit_LIS3DH.h>
 #include <Adafruit_Sensor.h>
 
-#define SUCCESS 0
-#define ERROR -1
-#define LED_PIN 6
+#define LED_PIN 1
 #define LED_COUNT 20
-#define ACCEL_ADDR 0x19
+#define ACCEL_ADDR 0x18
+#define ACCEL_SCL 2
+#define ACCEL_SDA 0
 #define ACCEL_RANGE LIS3DH_RANGE_4_G
 #define ACCEL_READINGS 5
 #define GRAVITY 9.8
-#define CHANGE_COLOR 24
+#define CHANGE_COLOR 8
 #define MAX_STATIONARY 248
 #define MAX_SPEED 24
 #define MAX_FREQ 24
@@ -40,25 +41,24 @@ typedef struct  {
 
 firectx ctx[LED_COUNT] = {0};
 CRGB strip[LED_COUNT];
-Adafruit_LIS3DH accel = Adafruit_LIS3DH(&Wire1);
+Adafruit_LIS3DH accel = Adafruit_LIS3DH();
 float max_sensor_value = 0.0;
 moving_avg avg = {0};
 
-int setContext(
+void setContext(
     firectx* ctx,
     uint8_t default_brightness,
     uint8_t color,
     uint8_t index
 );
-int sinWave(firectx* ctx);
-int setPixelColor(firectx* ctx, uint8_t color);
+void sinWave(firectx* ctx);
+void setPixelColor(firectx* ctx, uint8_t color);
 float calculateMagnitude();
 
 void setup() {
     // Need to use TinyUSB stack for this to work.
     Serial.begin(115200);
-    Serial.println("Starting flora...");
-    int8_t error = 0;
+    Serial.println("Starting gemma...");
     if (!accel.begin(ACCEL_ADDR)) {
         while(true){
             Serial.println("Couldn't find the ACCELEROMETER!");
@@ -79,17 +79,13 @@ void setup() {
     }
     FastLED.addLeds<NEOPIXEL, LED_PIN>(strip, LED_COUNT);
     for (uint8_t i = 0; i < LED_COUNT; i++) {
-
         strip[i] = CRGB::Red;
-        error = setContext(
+        setContext(
             &ctx[i],
             MAX_STATIONARY/2,
             random(HUE_RED, HUE_ORANGE),
             i
         );
-        if (error < SUCCESS) {
-            foreverError();
-        }
     }
 
     FastLED.show();
@@ -103,8 +99,7 @@ void loop() {
 
     float mag = calculateMagnitude();
     for (uint8_t idx = 0; idx < LED_COUNT; idx++) {
-        error = sinWave(&ctx[idx]);
-        if (error < SUCCESS) foreverError();
+        sinWave(&ctx[idx]);
 
         if ((ctx[idx].brightness <= CHANGE_COLOR || mag >= 0.20 )
           && ctx[idx].toggledColor == false) {
@@ -126,8 +121,7 @@ void loop() {
             color = ctx[idx].color;
             ctx[idx].toggledColor = false;
         }
-        error = setPixelColor(&ctx[idx], color);
-        if (error < SUCCESS) foreverError();
+        setPixelColor(&ctx[idx], color);
     }
 
     FastLED.show();
@@ -141,7 +135,7 @@ void foreverError() {
     }
 }
 
-int setContext(
+void setContext(
     firectx* ctx,
     uint8_t default_brightness,
     uint8_t color,
@@ -154,10 +148,9 @@ int setContext(
     ctx->freq = uint8_t(random(1,24));
     ctx->toggledColor = false;
     setPixelColor(ctx, color);
-    return SUCCESS;
 }
 
-int sinWave(firectx* ctx) {
+void sinWave(firectx* ctx) {
     ctx->phase += ctx->speed;
     ctx->brightness = quadwave8(ctx->freq*ctx->phase);
     if (ctx->brightness > MAX_STATIONARY) {
@@ -166,14 +159,12 @@ int sinWave(firectx* ctx) {
         ctx->brightness = CHANGE_COLOR;
     }
     Serial.println(ctx->brightness);
-    return SUCCESS;
 }
 
-int setPixelColor(firectx* ctx, uint8_t color) {
+void setPixelColor(firectx* ctx, uint8_t color) {
     ctx->color = color;
     // TODO: I want to break this out at some point...
     strip[ctx->index] = CHSV(ctx->color, 255, ctx->brightness);
-    return SUCCESS;
 }
 
 float calculateMagnitude() {
